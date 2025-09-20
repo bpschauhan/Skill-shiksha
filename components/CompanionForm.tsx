a638f3d204338c30d49a2ac4cbe3fbccac70fb9b
Bhanu Pratap Singh Chauhan <bhanupratapsinghchauhan945800@gmail.com>
2025-09-20 13:31:00 +0530

    add authentication

--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -24,6 +24,7 @@ import {
 import {subjects} from "@/constants";
 import {Textarea} from "@/components/ui/textarea";
 import {redirect} from "next/navigation";
+import {createCompanion} from "@/lib/actions/companion.action";
 
 const formSchema = z.object({
     name: z.string().min(1, { message: 'Companion is required.'}),
@@ -47,8 +48,15 @@ const CompanionForm = () => {
         },
     })
 
-    const onSubmit = (values: z.infer<typeof formSchema>) => {
-        console.log(values)
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        const companion = await createCompanion(values);
+
+        if(companion) {
+            redirect(`/companions/${companion.id}`);
+        } else {
+            console.log('Failed to create a companion');
+            redirect('/');
+        }
     }
 
     return (
